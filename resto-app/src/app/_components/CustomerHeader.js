a30018494614a9dcf6665450184e49351d917af4
Laptop Wala <muhammadomairakbar@gmail.com>
2024-05-02 05:51:22 +0500

    add to cart

--- resto-app/src/app/_components/CustomerHeader.js
+++ resto-app/src/app/_components/CustomerHeader.js
@@ -4,23 +4,52 @@ import { useState, useEffect } from 'react'
 
 const CustomerHeader = (props) => {
 
-  const cartStorage =  JSON.parse(localStorage.getItem('cart'));
-  const [cartNumber, setCartNumber] = useState(cartStorage?.length);
-  const [cartItem, setCartItem] = useState();
+  // const cartStorage = JSON.parse(localStorage.getItem('cart'));
+  const [cartNumber, setCartNumber] = useState();  //cartStorage?.length
+  const [cartItem, setCartItem] = useState();  //cartStorage
+  const [cartArray, setCartArray] = useState([]);
 
-  useEffect(()=>{
-    if(props.cartData){
-          if(cartNumber){
+  const addItemToArray = (item) => {
+    setCartArray([...cartArray, item]);
+  };
 
-    }else{
-      setCartNumber(1)
-      setCartItem([props.cartData])
-      
-}
-  localStorage.setItem('cart',JSON.stringify([props.cartData]));
+  useEffect(() => {
+    if (props.cartData) {
+      if (cartNumber) {
+        console.log(props.cartData)
+        setCartNumber(cartNumber + 1)
+        let localCart = JSON.parse(localStorage.getItem('cart'))
+        localCart.push(props.cartData);
+        setCartArray(localCart);
+        // setCartArray(cartArray.push(props.cartData));
+        console.log(localCart)
+        console.log("((((((((())))))))")
+        console.log(cartArray)
+        localStorage.setItem('cart', JSON.stringify(localCart))
+      } else {
+        setCartNumber(1);
+        console.log(props.cartData)
+        localStorage.setItem('cart', JSON.stringify([props.cartData]))
+      }
+
+
+
+
+      // if (cartNumber) {
+      //   addItemToArray(props.cartData);
+      //   setCartItem(myArray);
+      //   setCartNumber(cartNumber+1)
+      //   localStorage.setItem('cart', JSON.stringify(myArray))
+
+      // } else {
+      //   setCartNumber(1)
+      //   setCartItem([props.cartData])
+      //   localStorage.setItem('cart', JSON.stringify([props.cartData]));
+
+      // }
 
     }
-  },[props.cartData])
+  }, [props.cartData])
 
 
   return (
@@ -31,7 +60,7 @@ const CustomerHeader = (props) => {
       <div>
         <ul className='uls'>
           <li className='lis'><Link href="/">Home</Link></li>
-          <li className='lis'><Link href="#">Cart({cartNumber})</Link></li>
+          <li className='lis'><Link href="#">Cart({cartNumber ? cartNumber : 0})</Link></li>
           <li className='lis'><Link href="#">Contact us</Link></li>
           <li className='lis'><Link href="/restaurant">Add restaurant</Link></li>
           <li className='lis'><Link href="/restaurant"><button className='header-button' >Login</button></Link></li>
